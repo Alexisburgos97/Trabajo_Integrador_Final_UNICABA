bdc865bc6b0854ea0f111fa5808b5a10f57c4a6c
caromifre <kafmisio@gmail.com>
2025-11-10 18:47:46 -0300

    menues interconectadosy funcionales

--- Assets/scripts/Menu/Menu_inicio.cs
+++ Assets/scripts/Menu/Menu_inicio.cs
@@ -1,62 +1,71 @@
+using Simplon;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.UI;
-using Simplon;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuLoader : MonoBehaviour
 {
+    //SOUND CONTROLLER
+    SoundController soundController;
+
+    //SOUNDS
+    [SerializeField] private AudioClip Music;
+    //[SerializeField] private AudioClip ClickSound;
+
     //[SerializeField] GameObject _PauseMenu;
     bool _isPaused;
 
     [SerializeField] private Button _playButton;
     [SerializeField] private Button _Pausa;
-    [SerializeField] private Button _controlsButton;
+    [SerializeField] private Button _OpcionesButton;
+    [SerializeField] private Button _ControlButton;
+    [SerializeField] private Button _SonidoButton;
+    [SerializeField] private Button _AccesibilidadButton;
+    [SerializeField] private Button _CreditoButton;
     [SerializeField] private Button _quitButton;
     [SerializeField] private Button _toMainMenuButton;
-    //[SerializeField] private GameObject controlPanel;
-    //[SerializeField] private GameObject hudInfo;
 
-
-
-
+    [Header("Botones a controlar")]
+    [SerializeField] private List<Button> botones = new List<Button>();
 
     // Start is called before the first frame update
     private void Awake()
     {
         _playButton?.onClick.AddListener(LoadFirstLevel);
-        _controlsButton?.onClick.AddListener(OpenControls);
+        _OpcionesButton?.onClick.AddListener(OpenOpciones);
+        _ControlButton?.onClick.AddListener(OpenControles);
+        _SonidoButton?.onClick.AddListener(OpenSonido);
+        _AccesibilidadButton?.onClick.AddListener(OpenAccesibilidad);
+        _CreditoButton?.onClick.AddListener(OpenCretido);
         _quitButton?.onClick.AddListener(OnQuit);
         _toMainMenuButton?.onClick.AddListener(ToMainMenu);
         _Pausa?.onClick.AddListener(check_pausa);
-        
+    }
+
+    private void Start()
+    {
+        soundController = GetComponent<SoundController>();
+        if (Music != null)
+        {
+            soundController.PlayMusic(Music);
+        }
     }
 
     private void OnDestroy()
     {
         _playButton?.onClick.RemoveAllListeners();
-        _controlsButton?.onClick.RemoveAllListeners();
+        _OpcionesButton?.onClick.RemoveAllListeners();
+        _ControlButton?.onClick.RemoveAllListeners();
+        _SonidoButton?.onClick.RemoveAllListeners();
+        _AccesibilidadButton?.onClick.RemoveAllListeners();
+        _CreditoButton?.onClick.RemoveAllListeners();
         _quitButton?.onClick.RemoveAllListeners();
         _toMainMenuButton?.onClick.RemoveAllListeners();
         _Pausa?.onClick.RemoveAllListeners();
     }
 
-
-    // Update is called once per frame
-    /*
-    void Update()
-    {
-        if (Input.GetButtonDown("Pause"))
-        {
-            if (!isPaused) {
-                OnPause();
-            } else {
-                OnResume();
-            }
-        }
-
-    }
-    */
     public void check_pausa()
 {
     if (!_isPaused)
@@ -75,42 +84,42 @@ public class MenuLoader : MonoBehaviour
         GameControler.Instance.PasarNivel("Inicio");
         GameControler.Instance.ResetVariables();
         Time.timeScale = 1.0f;
-        //hudInfo?.gameObject.SetActive(false);//ya no se usa
     }
-    public void OpenControls() {
-        //controlPanel?.SetActive(true);
+    public void OpenOpciones() {
         GameControler.Instance.PasarNivel("Opciones");
     }
+    public void OpenControles()
+    {
+        GameControler.instance.PasarNivel("Controles");
+    }
 
+    public void OpenSonido()
+    {
+        GameControler.instance.PasarNivel("Sonido");
+    }
+    public void OpenAccesibilidad()
+    {
+        //GameControler.instance.PasarNivel("Controles");
+    }
+    public void OpenCretido()
+    {
+        GameControler.instance.PasarNivel("Creditos");
+    }
     public void LoadFirstLevel() {
         //craga la primer pista de carreras
         GameControler.Instance.PasarNivel("Nivel1");
         GameControler.Instance.AgregarnEscena("Hud");
-        //hudInfo?.gameObject.SetActive(true);//ya no se usa
     }
-    //anule esta funcion ya que ahora se usa desde el 
-    //game manager
-    /*public void LoadLevel(int level) {
-        SceneManager.LoadScene(level);
-    }*/
 
      public void OnPause() {
-        //hudInfo?.gameObject.SetActive(false);//ya no se usa
-        //controlPanel?.SetActive(false);
-        //PauseMenu?.SetActive(true);
         _isPaused = true;
         Time.timeScale = 0f;
-
     }
+
     public void OnResume() {
         print("sin pausa");
         Time.timeScale = 1f;
-        //hudInfo?.gameObject.SetActive(true);//ya no se usa
-        //PauseMenu?.SetActive(false);
-        //controlPanel?.SetActive(false);
         _isPaused = false;
-        //mostrar el hud del auto
-        //GameControler.instance.agregarnEscena("Hud");
     }
     public void OnQuit() {
        Application.Quit();
@@ -118,7 +127,8 @@ public class MenuLoader : MonoBehaviour
                 EditorApplication.ExitPlaymode();
         #endif
     }
-    
+ 
+
    void UnloadPauseScene()
     {
       SceneManager.UnloadSceneAsync("Pausa");
---
+++ Assets/scripts/Sound/SoundController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour, ISoundController
+{
+    [SerializeField] private AudioSource audioSourceSFXOnce;
+    [SerializeField] private AudioSource audioSourceSFXLoop;
+    [SerializeField] private AudioSource audioSourceMusicLoop;
+
+    void Start()
+    {
+        //SFX
+        if (audioSourceSFXOnce != null)
+        {
+            audioSourceSFXOnce.volume = PlayerPrefs.GetFloat("SFX", 0.3f);
+        }
+        //--LOOP SFX (Esto no se usa ni en pedo)
+        if (audioSourceSFXLoop != null)
+        {
+            audioSourceSFXLoop.volume = PlayerPrefs.GetFloat("SFX", 0.3f);
+        }
+        //MUSIC
+        if (audioSourceMusicLoop != null)
+        {
+            audioSourceMusicLoop.volume = PlayerPrefs.GetFloat("Music", 0.3f);
+        }
+    }
+
+    private void Update()
+    {
+        if (audioSourceSFXOnce != null)
+        {
+            audioSourceSFXOnce.volume = PlayerPrefs.GetFloat("SFX", 0.3f);
+        }
+        if (audioSourceSFXLoop != null)
+        {
+            audioSourceSFXLoop.volume = PlayerPrefs.GetFloat("SFX", 0.3f);
+        }
+        if (audioSourceMusicLoop != null)
+        {
+            audioSourceMusicLoop.volume = PlayerPrefs.GetFloat("Music", 0.3f);
+        }
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        audioSourceSFXOnce.loop = false;
+        audioSourceSFXOnce.PlayOneShot(clip);
+    }
+
+    public void PlaySFXLoop(AudioClip clip)
+    {
+        audioSourceSFXLoop.loop = true;
+        audioSourceSFXLoop.clip = clip;
+        audioSourceSFXLoop.Play();
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        audioSourceMusicLoop.loop = true;
+        audioSourceMusicLoop.clip = clip;
+        audioSourceMusicLoop.Play();
+    }
+
+    public void StopLoopSound()
+    {
+        audioSourceMusicLoop.Stop();
+        audioSourceSFXLoop.Stop();
+    }
+}
